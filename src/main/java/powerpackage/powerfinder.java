2d14a094b8fb066e0616f9f86a02a4e014798986
Boomi82 <101126344+Boomi82@users.noreply.github.com>
2022-03-20 04:33:51 +0530

    Corrected the naming conventions

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,6 +1,7 @@
 package powerpackage;
 
-public class powerfinder {
+/*
+public class Powerfinder {
     public static int OF(int xyz, int pqr) {
         int p = 1;
         for (int i = 0; i<pqr; i++) {
@@ -9,3 +10,14 @@ public class powerfinder {
         return p;
     }
 }
+*/
+public class PowerFinder {
+    public static int calculationOfPower(int base, int exponent) {
+        int result = 1;
+        while (exponent != 0) {
+            result *= base;
+            exponent--;
+        }
+
+        return result;
+    }
